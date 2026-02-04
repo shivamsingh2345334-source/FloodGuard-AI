793eb9eaeaf63645d1d0ab772ee8653155fd6b46
shivamsingh2345334-source <shivamsingh2345334@gmail.com>
2026-02-04 20:14:07 +0530

    response_agent.py

---
+++ response_agent (1).py
@@ -0,0 +1,23 @@
+
+def response_agent(risk_level):
+
+    if risk_level == "High":
+        return {
+            "alert": "🚨 Immediate Evacuation Required",
+            "resources": ["Rescue Boats", "Medical Teams", "Shelters"],
+            "action": "Evacuate within 6 hours"
+        }
+
+    elif risk_level == "Medium":
+        return {
+            "alert": "⚠️ Flood Warning",
+            "resources": ["Ambulances", "Rescue Teams"],
+            "action": "Prepare evacuation + monitor situation"
+        }
+
+    else:
+        return {
+            "alert": "✅ Normal Conditions",
+            "resources": ["Monitoring Teams"],
+            "action": "Continue surveillance"
+        }
